23f355ebd3619570cb5a4276c94608155b976246
Plirexic <92991314+Plirexic@users.noreply.github.com>
2025-04-15 00:46:04 +0200

    project skeleton

---
+++ studybackend/src/main/java/de/hsesslingen/studybackend/StudybackendApplication.java
@@ -0,0 +1,13 @@
+package de.hsesslingen.studybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudybackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudybackendApplication.class, args);
+	}
+
+}
---
+++ studybackend/src/test/java/de/hsesslingen/studybackend/StudybackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hsesslingen.studybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudybackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
